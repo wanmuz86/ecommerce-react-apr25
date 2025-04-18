b7c656b89599c268acc69767529ea14ea79264ab
Wan Muzaffar <wanmuz86@gmail.com>
2025-04-18 14:57:40 +0800

    to verify in cart

--- src/pages/cart/Cart.jsx
+++ src/pages/cart/Cart.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
 import './Cart.css'
-
+import {useCart} from '../../context/CartContext.jsx'
 const Cart = () => {
+
+  const {state, dispatch} = useCart()
+
   return (
-    <div>Cart</div>
+    <div>
+      <h2>Your Cart</h2>
+      <div className="container">
+        {
+          state.cart.map(val=><div><h2>{val.item.title} - {val.quantity}</h2></div>)
+        }
+      </div>
+    </div>
   )
 }
 
