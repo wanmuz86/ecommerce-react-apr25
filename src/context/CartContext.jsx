25cfc6c71b2a61e904e490dcc2478f58e222e08d
Wan Muzaffar <wanmuz86@gmail.com>
2025-04-18 14:29:59 +0800

    add the 'delete item' logic

--- src/context/CartContext.jsx
+++ src/context/CartContext.jsx
@@ -2,53 +2,71 @@ import { createContext } from "react";
 
 const CartContext = createContext();
 
-const CartProvider = ({children}) => {
+const CartProvider = ({ children }) => {
     const initialState = {
-        cart:[]
+        cart: []
     }
 
-    const cartReducer = (state,action) => {
+    const cartReducer = (state, action) => {
         // We will define all the actions on the state
-        switch (action.type){
+        switch (action.type) {
             case 'ADD_ITEM':
-                     // if item already exists in the cart, add the quantity by 1
-                     // Find the index of the given item id, if exists , I will get the index, if not i will get -1
+                // if item already exists in the cart, add the quantity by 1
+                // Find the index of the given item id, if exists , I will get the index, if not i will get -1
                 const existingItemIndexAdd = state.cart.findIndex(item => item.item.id === action.payload.item.id);
 
-                if (existingItemIndexAdd !== -1){
+                if (existingItemIndexAdd !== -1) {
                     // Array Spread operator, copy in updatedCart
                     const updatedCart = [...state.cart];
-                    updatedCart[existingItemIndexAdd].quantity++ ; // Increment the quantity of the item
+                    updatedCart[existingItemIndexAdd].quantity++; // Increment the quantity of the item
                     // Update the state with the new item quantity
                     return {
                         ...state,
-                        cart:updatedCart 
+                        cart: updatedCart
                     }
 
                 }
                 else {
 
-                // if not, add it in the cart, and set the quantity to 1
-                return {
-                    ...state,
-                    cart:[...cart,action.payload]
+                    // if not, add it in the cart, and set the quantity to 1
+                    return {
+                        ...state,
+                        cart: [...cart, action.payload]
+                    }
                 }
-                }
-           
+
 
 
             case 'REMOVE_ITEM':
 
             // if item is already in the cart
-             // if the item quantity is > 1, remove the quantity by1
+            const existingItemIndexDel = state.cart.findIndex(val => val.item.id === action.payload.id)
+            if (existingItemIndexDel !== -1){
+            // if the item quantity is > 1, remove the quantity by1
+            const updatedCart = [...state.cart];
+            if (updatedCart[existingItemIndexDel].quantity > 1){
+                updatedCart[existingItemIndexDel].quantity--
+                return {
+                    ...state,
+                    cart:updatedCart
+                }
+            }
+            else {
+                 // if it is equal to 1 , remove the cart
+                 return {
+                    ...cart,
+                    cart:state.cart.filter(val=> val.item.id !== action.payload.item.id)
+                 }
 
-             // if it is equal to 1 , remove the cart
-
-            case 'EMPTY_CART':
-                 // Modify the start by updating the cart to []
-            return {... state, 
-                cart:[]
+            }
             } 
+            
+            case 'EMPTY_CART':
+                // Modify the start by updating the cart to []
+                return {
+                    ...state,
+                    cart: []
+                }
 
 
             default:
@@ -59,4 +77,4 @@ const CartProvider = ({children}) => {
 
 }
 
-export {CartProvider}
+export { CartProvider }
