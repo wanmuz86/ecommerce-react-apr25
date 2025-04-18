8290fcd2c8b99c19f9d009941aca27b96cc69ec6
Wan Muzaffar <wanmuz86@gmail.com>
2025-04-18 09:50:52 +0800

    add header and footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,18 @@
 
 import { Outlet } from 'react-router-dom'
 import './App.css'
+import Header from './components/header/Header'
+import Footer from './components/footer/Footer'
 
 function App() {
 
 
   return (
     <>
-     <h1>E commerce app</h1>
+     <Header/>
      {/* the content of the component will go here */}
      <Outlet/>
+     <Footer/>
     </>
   )
 }
